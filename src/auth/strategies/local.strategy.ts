4f45d7769e313818979e05c42da64b67f6bc60f1
Pini Che <pinich@gmail.com>
2020-07-15 20:11:45 +0300

    - Request-scoped strategies

--- src/auth/strategies/local.strategy.ts
+++ src/auth/strategies/local.strategy.ts
@@ -2,6 +2,7 @@ import { Strategy } from 'passport-local';
 import { PassportStrategy } from '@nestjs/passport';
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { AuthService } from '../auth.service';
+import { ModuleRef, ContextIdFactory } from '@nestjs/core';
 
 @Injectable()
 export class LocalStrategy extends PassportStrategy(Strategy) {
@@ -9,8 +10,8 @@ export class LocalStrategy extends PassportStrategy(Strategy) {
      *
      * @param authService the Auth Service
      */
-    constructor(private authService: AuthService) {
-        super();
+    constructor(private moduleRef: ModuleRef) {
+        super({ passReqToCallback: true });
     }
 
     /**
@@ -18,8 +19,22 @@ export class LocalStrategy extends PassportStrategy(Strategy) {
      * @param username
      * @param pass
      */
+    /*
     async validate(username: string, pass: string) {
         const user = await this.authService.validateUser(username, pass);
+        if (!user) {
+            throw new UnauthorizedException();
+        }
+        return user;
+    }
+    */
+    async validate(request: Request, username: string, password: string) {
+        const contextId = ContextIdFactory.getByRequest(request);
+        const authService = await this.moduleRef.resolve(
+            AuthService,
+            contextId,
+        );
+        const user = await authService.validateUser(username, password);
         if (!user) {
             throw new UnauthorizedException();
         }
