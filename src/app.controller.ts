2511f2e72e42b7dee249a3c4bec780b5af4d1776
Pini Che <pinich@gmail.com>
2020-07-15 19:41:32 +0300

    Having issue with running

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,11 +1,22 @@
 import { Controller, Get, UseGuards, Request, Post } from '@nestjs/common';
 import { LocalAuthGuard } from './auth/local-auth.guard';
+import { AuthService } from './auth/auth.service';
+import { JwtAuthGuard } from './auth/jwt-auth.guard';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller()
 export class AppController {
+    constructor(private authService: AuthService) {}
+
     @UseGuards(LocalAuthGuard)
     @Post('auth/login')
     async login(@Request() req) {
+        return this.authService.login(req.user);
+    }
+
+    @UseGuards(JwtAuthGuard)
+    @Get('profile')
+    getProfile(@Request() req) {
         return req.user;
     }
 }
