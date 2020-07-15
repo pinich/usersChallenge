669d921e95c29278f191520dc8a9ee45807ef54e
Pini Che <pinich@gmail.com>
2020-07-15 15:41:54 +0300

    - Created the Auth/User modules and services

---
+++ src/user/user.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@nestjs/common';
+
+export type User = any;
+
+@Injectable()
+export class UserService {
+    private readonly users: User[];
+
+    constructor() {
+        this.users = [
+            {
+                userId: 1,
+                username: 'john',
+                password: 'changeme',
+            },
+            {
+                userId: 2,
+                username: 'chris',
+                password: 'secret',
+            },
+            {
+                userId: 3,
+                username: 'maria',
+                password: 'guess',
+            },
+        ];
+    }
+}
