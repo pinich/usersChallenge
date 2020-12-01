291296a0ddc7d656a5532b909e3007d73dcb2d58
Pini Che <pinich@gmail.com>
2020-12-01 20:33:35 +0200

    - added support to dotenv - Using ValidationPipe

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,11 +1,17 @@
-import { Logger } from '@nestjs/common';
+require('dotenv').config();
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const port = 3000;
+  const port = process.env.PORT || 3000;
   const app = await NestFactory.create(AppModule);
-  Logger.log(`Listening on port ${port}`,'Main')
+  
+  const globalPrefix = 'api';
+  app.setGlobalPrefix(globalPrefix);
+  app.useGlobalPipes(new ValidationPipe());
+
+  Logger.log(`Listening on port ${port}`, 'Main')
   await app.listen(port);
 }
 bootstrap();
